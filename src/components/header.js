296a32ac3e7d34a0ae5242ad4dd0820a87afd778
Pams <pamelapardo@Pamss-MacBook-Air.local>
2023-07-11 09:20:15 +0200

    conception header

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './header.scss'
+import Logo from '../images/Arkhe_logo.png'
+
+export default function Header() {
+  return (
+    <div className='background_header'>
+      <div>
+        <img src={Logo} alt="Arkhe_logo"/>
+      </div>
+
+      <div className='tab_container'>
+        <div>Tab app</div>
+        <div>Tab Wiki</div>
+      </div>
+
+      <div>
+        <div>User</div>
+      </div>
+    </div>
+  )
+}
