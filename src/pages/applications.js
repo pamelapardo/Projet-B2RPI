1b9cc229ba07ae9fc24c358ef6906a7c9637f7c1
Pams <pamelapardo@Pamss-MacBook-Air.local>
2023-09-03 15:56:38 +0200

    changed appCard styles to a better format

--- src/pages/applications.js
+++ src/pages/applications.js
@@ -4,28 +4,34 @@ import AppCard from '../components/componentApplications/appCard'
 import AppSearchBar from '../components/componentApplications/appSearchBar'
 import { db } from '../firebase';
 import { collection, getDocs } from "firebase/firestore";
-import { Link } from 'react-router-dom';
 
+// const appList = [
+//   { logo: 'logo', title: 'Application 1', description: 'This is my app description in a few words.' },
+//   { logo: 'logo', title: 'Application 2', description: 'This is my app description in a few words.' },
+//   { logo: 'logo', title: 'Application 3', description: 'This is my app description in a few words.' },
+//   { logo: 'logo', title: 'Application 4', description: 'This is my app description in a few words.' },
+//   { logo: 'logo', title: 'Application 5', description: 'This is my app description in a few words.' },
+//   { logo: 'logo', title: 'Application 6', description: 'This is my app description in a few words.' },
+// ]
 
 export default function Applications() {
 
   const [posts, setPosts] = useState([]);
 
   const fetchPost = async () => {
-
-    await getDocs(collection(db, "Apps"))
-      .then((querySnapshot) => {
-        const newData = querySnapshot.docs
-          .map((doc) => ({ ...doc.data(), id: doc.id }));
-        setPosts(newData);
-        console.log(posts, newData);
-      })
-
+    await getDocs(collection(db, "Apps")).then((querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setPosts(newData);
+      console.log(posts, newData);
+    })
   }
 
   useEffect(() => {
     fetchPost();
   }, [])
+
+
+  
   return (
     <div className='app_page_container'>
       <div className='app_page_searchBar_container'>
