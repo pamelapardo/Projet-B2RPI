f21fa348fa49e49a7588eb1d7af138dcca34353e
rosierludivine <ludivine.rosier38@gmail.com>
2023-09-03 11:46:24 +0200

    reprise des données

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import Profile from './pages/profile';
 import MDEditorArticle from './components/mdEditorArticle';
 import AppLaunch from './components/componentApplications/appLaunch';
 // import Todo from './components/componentApplications/firebaseTest';
+import WikiBrouillons from './pages/wikiOutlets/wikiBrouillons';
+import Todo from './components/componentApplications/firebaseTest';
 
 function App() {
   //Gestion du  changement de status 
