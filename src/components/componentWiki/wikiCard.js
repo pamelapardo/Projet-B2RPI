6c6ce732ff8a2155c311626135d56abf65b22ef1
Pams <pamelapardo@Pamss-MacBook-Air.local>
2023-09-04 15:46:19 +0200

    retrieved data from firestore to css for articles types

--- src/components/componentWiki/wikiCard.js
+++ src/components/componentWiki/wikiCard.js
@@ -3,16 +3,20 @@ import './wikiCard.scss'
 import '../../components/componentApplications/appCard.scss'
 
 export default function WikiCard(articleSrc) {
+  const cardStyle = {
+    backgroundColor: articleSrc.articleType[articleSrc.article_type], // Assuming article_type is the field that holds the type information
+  };
+
   return (
     <div className='wikiCard_container'>
-      <div className='wikiCard_app_color'></div>
+      <div className='tout' style={cardStyle}></div>
       <div className='wikiCard_card_items'>
         <div className='wikiCard_titles'>
           <h3 className='card_app_title bottom_top_title_space'>{articleSrc.appTitle}</h3> <p className='card_app_title space_title'> - </p> <h3 className='card_app_title article_title_style'>{articleSrc.articleTitle}</h3>
         </div>
         <p className='wikiCard_app_description'>{articleSrc.articleDescription}</p>
         <div className='wikiCard_date_items'>
-          <p className='wikiCard_edition'>Dernière édition: </p> <p>{articleSrc.articleLastEdit}</p>
+          <p className='wikiCard_edition'>Dernière édition:</p> <p>{articleSrc.articleLastEdit}</p>
         </div>
       </div>
     </div>
--- src/pages/wikiOutlets/wikiContent.js
+++ src/pages/wikiOutlets/wikiContent.js
@@ -1,39 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import WikiSearchBar from '../../components/wikiSearchBar'
 import WikiCard from '../../components/componentWiki/wikiCard'
-
-const articleList = [
-  { appTitle: 'Application 1', articleTitle: 'This is my articles title', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '10/10/2023' },
-  { appTitle: 'AppCaatalist', articleTitle: 'Article title', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '14/10/2023' },
-  { appTitle: 'Larees V', articleTitle: 'How to solve this problem', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '14/10/2023' },
-  { appTitle: 'Lorem Ipsum', articleTitle: 'This problem', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '16/10/2023' },
-  { appTitle: 'App Test 1', articleTitle: 'Solution to this problem', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '16/10/2023' },
-  { appTitle: 'TMG Postes', articleTitle: 'This article documents this', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '17/10/2023' },
-  { appTitle: 'Teams', articleTitle: 'This article says how', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '17/10/2023' },
-  { appTitle: 'RH Space', articleTitle: 'Heres how', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '17/10/2023' },
-  { appTitle: 'IT ToolSet', articleTitle: 'Which is the best way', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '17/10/2023' },
-  { appTitle: 'One', articleTitle: 'Problem solved', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '22/10/2023' },
-  { appTitle: 'Notion', articleTitle: 'This title says everyting', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '22/10/2023' },
-  { appTitle: 'Economks', articleTitle: 'This is a resume', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '24/10/2023' },
-  { appTitle: 'Directory', articleTitle: 'Procedure to do this', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '26/10/2023' },
-  { appTitle: 'LAPS', articleTitle: 'How to get around that', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '26/10/2023' },
-  { appTitle: 'PuTTY', articleTitle: 'This is my experience', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '28/10/2023' },
-  { appTitle: 'Application 2', articleTitle: 'This is the way', articleDescription: 'This is a copytext loren ipsum text dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', articleLastEdit: '29/10/2023' }
-]
+import { db } from '../../firebase';
+import { collection, getDocs } from "firebase/firestore";
+import '../../components/componentApplications/appCard.scss'
 
 export default function WikiContent() {
+
+  const [articles, setArticles] = useState([]);
+  const articleType = {
+    Solution: '#A4C069',
+    Documentation: '#c096e0',
+    Procedure: '#45D7CE',
+  };
+
+  const fetchArticle = async () => {
+    await getDocs(collection(db, "Articles")).then((querySnapshot) => {
+      const mapArticles = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setArticles(mapArticles);
+    })
+  }
+
+  useEffect(() => {
+    fetchArticle();
+  }, [])
+
+
+
   return (
     <>
       <WikiSearchBar/>
       <div className='wiki_total_articles_container masked_overflow'>
-        {articleList.map((articleSrc, index) => {
-          return <WikiCard
-            key={index}
-            appTitle={articleSrc.appTitle}
-            articleTitle={articleSrc.articleTitle}
-            articleDescription={articleSrc.articleDescription}
-            articleLastEdit={articleSrc.articleLastEdit}
-          />
+        {articles.map((articleSrc, index) => {
+          return (
+            <WikiCard
+              key={index}
+              appTitle={articleSrc.article_app}
+              articleTitle={articleSrc.article_title}
+              articleDescription={articleSrc.article_desc}
+              articleLastEdit={articleSrc.article_lastEdit}
+              article_type={articleSrc.article_type}
+              articleType={articleType}
+            />
+          );
         })}
       </div>
     </>
