5f6bd011144c4fd510b7a4b6b732a87302f02b0f
Pams <pamelapardo@Pamss-MacBook-Air.local>
2023-09-09 23:12:18 +0200

    git merging problems

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Wiki from './pages/wiki';
 import WikiContent from './pages/wikiOutlets/wikiContent';
 import WikiLanding from './pages/wikiOutlets/wikiLanding';
 import Profile from './pages/profile';
+import WikiBrouillons from './pages/wikiOutlets/wikiBrouillons';
 import MDEditorArticle from './components/mdEditorArticle';
 import AppLaunch from './components/componentApplications/appLaunch';
 import Login from '../src/components/login'
@@ -41,18 +42,8 @@ function App() {
           <Route path="*/brouillons/" element={<WikiBrouillons />} />
           <Route path='*/redaction_article' element={<MDEditorArticle />} />
         </Route>
-        <Route path='/profile/' element={<Profile />} />
-        <Route path='/appLaunch' element={<AppLaunch />} />
-
-        {/* PAGES ADMIN - Route temporaire */}
-        <Route path='/admin-Tableau-de-bord/*' element={<AdminPages/>}>
-          <Route default path='*/tableau-de-bord' element={<TableauDeBord/>}/>
-          <Route path='*/gestion-de-utilisateurs' element={<GestionUtilisateurs/>}/>
-          <Route path='*/metiers' element={<Metiers/>}/>
-          <Route path='*/administration' element={<Administration/>}/>
-
-        </Route>
-
+        <Route path='/profile/' element={<Profile/>}/>
+        <Route path='/appLaunch' element={<AppLaunch/>}/>
       </Routes>
     </div>
   );
