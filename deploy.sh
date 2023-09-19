950f2ee0cdba6fd1517649210117f7e007095c7d
Pamela Pardo <76227952+pamelapardo@users.noreply.github.com>
2023-09-20 00:01:55 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,26 +15,19 @@ apt install -y git
 
 # Cloner le dépôt Git dans le répertoire du site Apache
 cd /var/www/html
-
-# Spécifiez ici l'URL de votre dépôt Git
 git clone https://github.com/pamelapardo/arkhe_build
 
-# Modifier le chemin du répertoire racine dans le fichier de configuration d'Apache
-nouveau_chemin="/var/www/html/arkhe_build"
-
-# Vérifier si le chemin existe
-if [ -d "$nouveau_chemin" ]; then
-    sed -i "s#/var/www/html#$nouveau_chemin#" /etc/apache2/sites-available/000-default.conf
-else
-    echo "Le chemin '$nouveau_chemin' n'existe pas."
-    exit 1
-fi
+# Modifier le chemin dans le fichier de configuration Apache
+ancien_chemin="/var/www/html"
+nouveau_chemin="/var/www/html/arkhe_built"
+fichier_config="/etc/apache2/sites-available/000-default.conf"
+sed -i "s|$ancien_chemin|$nouveau_chemin|g" "$fichier_config"
 
 # Redémarrer Apache pour appliquer les modifications
 systemctl restart apache2
 
 # Afficher un message de confirmation
-echo "Le chemin du répertoire racine d'Apache a été modifié avec succès vers $nouveau_chemin."
+echo "Le site a été déployé avec succès."
 
 # Sortir du script
 exit 0
