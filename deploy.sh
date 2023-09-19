c68badcf63269dd0e47df6391b0961cc151bdc34
Pamela Pardo <76227952+pamelapardo@users.noreply.github.com>
2023-09-19 09:30:59 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Vérifier que le script est exécuté en tant que superutilisateur (root)
+if [[ $EUID -ne 0 ]]; then
+   echo "Ce script doit être exécuté en tant que superutilisateur (root)."
+   exit 1
+fi
+
+# Installer Apache
+apt update
+apt install -y apache2
+
+# Installer Git
+apt install -y git
+
+# Cloner le dépôt Git dans le répertoire du site Apache
+cd /var/www/html
+git clone https://github.com/pamelapardo/Projet-B2RPI
+
+# Redémarrer Apache pour appliquer les modifications
+systemctl restart apache2
+
+# Afficher un message de confirmation
+echo "Le site a été déployé avec succès."
+
+# Sortir du script
+exit 0
