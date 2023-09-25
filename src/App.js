d9bbc3190cc40cd45065d9259283129e81f9e91f
rosierludivine <ludivine.rosier38@gmail.com>
2023-09-25 19:21:50 +0200

    Change methods for login

--- src/App.js
+++ src/App.js
@@ -18,47 +18,31 @@ import Metiers from './pages/adminOutlets/metiers';
 import Administration from './pages/adminOutlets/administration';
 import ModifiedPassword from './components/modifiedPassword';
 import ConnexionAdmin from './components/connexionAdmin';
+import ProfilsAdmin from './pages/adminOutlets/profilsAdmin';
 
 function App(props) {
   // Gestion du changement de status
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-<<<<<<< HEAD
   const [isLoggedInAdmin, setIsLoggedInAdmin] = useState(false);
-=======
-  const [userRole, setUserRole] = useState(null); // Ajoutez un état pour stocker le rôle de l'utilisateur
->>>>>>> d54a5f9 (Login with firebase roles)
 
   // Fonction pour savoir si la connexion est réussie et stocker le rôle
-  const LoginSuccess = (role) => {
-    console.log('LoginSuccess called with role:', role);
+  const LoginSuccess = () => {
     setIsLoggedIn(true);
-    setUserRole(role);
   };
 
-<<<<<<< HEAD
   const LoginSuccessAdmin  = () => {
     setIsLoggedInAdmin(true);
   };
-=======
->>>>>>> d54a5f9 (Login with firebase roles)
 
   return (
     <div className="h100">
       <Routes>
-<<<<<<< HEAD
-        {/* Si la connexion est reussite alors affiche le Header  */}
-        {!isLoggedIn && <Route path="/*" element={<Login onLoginSuccess={LoginSuccess} />} />}
-        {isLoggedIn && (<Route path="/*" element={<Header />} />)}
-        <Route path="/modifiedPassword" element={<ModifiedPassword/>} />
-        <Route path="/loginAdmin" element={<ConnexionAdmin/>} />
-=======
         {/* Si la connexion est réussie alors affiche le Header */}
-        {!isLoggedIn ? (
-          <Route path="/" element={<Login onLoginSuccess={LoginSuccess} />} />
+        {!isLoggedIn  ? (
+          <Route path="/" element={<Login onLoginSuccess={LoginSuccess} onLoginSuccessAdmin={LoginSuccessAdmin}/>   } />
         ) : (
           <Route path="/*" element={<Header />} />
         )}
->>>>>>> d54a5f9 (Login with firebase roles)
       </Routes>
       <Routes>
         {isLoggedIn && ( // Afficher les routes protégées uniquement si l'utilisateur est connecté
@@ -73,36 +57,27 @@ function App(props) {
             <Route path='/profile/' element={<Profile />} />
             <Route path='/appLaunch' element={<AppLaunch />} />
 
-<<<<<<< HEAD
-        <Routes>
-        {/* Si la connexion est reussite alors affiche le Header  */}
-        {!isLoggedInAdmin && <Route path="/loginAdmin" element={<ConnexionAdmin onLoginSuccess={LoginSuccessAdmin} />} />}
-        {isLoggedInAdmin && (<Route path="/*" element={<Header />} />)}
-        <Route path="/modifiedPassword" element={<ModifiedPassword/>} />
-        <Route path="/*" element={<Login/>} />
-      </Routes>
-        {/* PAGES ADMIN - Route temporaire */}
-        <Route path='/admin-Tableau-de-bord/*' element={<AdminPages />}>
-          <Route default path='*/tableau-de-bord' element={<TableauDeBord />} />
-          <Route path='*/gestion-de-utilisateurs' element={<GestionUtilisateurs />} />
-          <Route path='*/metiers' element={<Metiers />} />
-          <Route path='*/administration' element={<Administration />} />
-
-        </Route>
-=======
             {/* PAGES ADMIN - Route temporaire */}
-            {userRole === 'admin' && ( // Afficher les routes admin uniquement si l'utilisateur a le rôle d'admin
-              <Route path='/admin-Tableau-de-bord/*' element={<AdminPages />}>
-                <Route default path='*/tableau-de-bord' element={<TableauDeBord />} />
+            <Routes>
+        {/* Si la connexion est réussie alors affiche le Header */}
+        {!isLoggedInAdmin  ? (
+          <Route path="/" element={<Login onLoginSuccessAdmin={LoginSuccessAdmin}/>   } />
+        ) : (
+          <Route path='/admin-Tableau-de-bord/*' element={<AdminPages />} />
+        )}
+      </Routes>
+            {isLoggedInAdmin &&( // Afficher les routes admin uniquement si l'utilisateur a le rôle d'admin
+              <Routes >
+                <Route default path='*/tableau-de-bord' element={<TableauDeBord />} >
                 <Route path='*/gestion-de-utilisateurs' element={<GestionUtilisateurs />} />
                 <Route path='*/profil-de-utilisateur' element={<ProfilsAdmin />} />
                 <Route path='*/metiers' element={<Metiers />} />
                 <Route path='*/administration' element={<Administration />} />
-              </Route>
+                </Route>
+              </Routes>
             )}
           </>
         )}
->>>>>>> d54a5f9 (Login with firebase roles)
       </Routes>
     </div>
   );
--- src/components/login.js
+++ src/components/login.js
@@ -1,44 +1,37 @@
 import './login.scss';
 import React, { useState } from 'react';
-import { Link, Navigate, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase'; // Importez le service d'authentification de votre fichier auth.js
-import { getFirestore, doc, getDoc } from 'firebase/firestore'; // Importez Firestore
 
 function Login(props) {
-  // Déclarer l'etat de email  
   const [email, setEmail] = useState('');
-  // Déclarer l'etat du mot de passe 
   const [password, setPassword] = useState('');
 
-  // Envoie du formulaire 
   const handleLogin = async (e) => {
-    // Empecher le rafraichissement de la page 
     e.preventDefault();
 
     try {
       // Utilisez le service d'authentification importé pour vous connecter
       const userCheck = await signInWithEmailAndPassword(auth, email, password);
-
-      // Vérifiez si l'utilisateur est administrateur (adapté à votre structure de données)
-      if (userCheck.user.email === 'admin@mail.com' && userCheck.user.isAdmin) {
-        // Redirigez l'administrateur vers la page admin
-        return <Navigate to='/admin-Tableau-de-bord/*'/>;
-      }
-
-      // Connexion réussie pour un utilisateur normal
-      console.log('Logged in as user:', userCheck.user.email);
+      // Connexion réussie
+      console.log('Logged in as:', userCheck.user.email);
       props.onLoginSuccess();
+      if (email === 'admin@mail.com') {
+        props.onLoginSuccessAdmin(); 
+      } else {
+        console.log("vous n'etes pas un administrateur")
+      }
     } catch (error) {
       // Gérer les erreurs d'authentification
       console.error('Login failed:', error.message);
     }
   };
 
+
   return (
     <div className="container">
       <h2>ARKHE</h2>
-       <p>Connexion Utilisateur </p>
       <form onSubmit={handleLogin}>
         <div className="form-group">
           <label htmlFor="email">E-mail:</label>
@@ -60,11 +53,9 @@ function Login(props) {
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button className='.common_btn' type="submit">Se connecter</button>
-        <br/>
-      <Link to="/modifiedPassword">Mot de passe oublié?</Link>
+        <button className='btn-Submit' type="submit">Se connecter</button>
       </form>
-      <Link to="/loginAdmin">Se connecter en tant qu’administrateur </Link>
+      <Link to="/forgot-password">Mot de passe oublié?</Link>
     </div>
   );
 }
